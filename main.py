7af0a3c5ef4e6014f345e9a23ebc4aa587968bdd
Guilherme Portugal <172698670+guilhptg@users.noreply.github.com>
2025-02-11 00:46:40 -0300

    update

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ import plotly.express as px
 import streamlit as st
 
 
-print('Hello world')
 
-df = pd.read_csv('supermarket_sales.csv', sep=';', decimal=',')
-# display(df)
-# df.info()
+# Faturamento por unidade
+# Tipo de produto mais vendodo, contribuição por filial
+# Desempenho das formas de pagamento
+# Como estão as avaliações das filiais
+
+
+
+st.set_page_config(layout='wide')
+
+def csv_to_df(df):
+    '''
+    Transform .csv files to Pandas DataFrame
+
+    :param csv file 
+    '''
+    df = pd.read_csv(df, sep=';', decimal=',')
+    df.info()
+    return df
+
+# DADOS
+
+df = csv_to_df('supermarket_sales.csv')
+df
 
 # Tratamento dos dados
 df['Date'] = pd.to_datetime(df['Date'])
@@ -23,26 +42,37 @@ df_filtred = df[df['Month'] == month]
 
 
 
-# Faturamento por unidade
-# Tipo de produto mais vendodo, contribuição por filial
-# Desempenho das formas de pagamento
-# Como estão as avaliações das filiais
-
-
+# Graficos da primeira linha
 col1, col2 = st.columns(2)
+
+# Graficos da segunda linha
 col3, col4, col5 = st.columns(3)
+
 
 # Faturamento por unidade
 fig_date = px.bar(df_filtred, x='Date', y='Total', color='City', title='Faturamento por dia')
-col1.plotly_chart(fig_date)
+col1.plotly_chart(fig_date, use_container_width=True)
 
 
 # Faturamento por tipo de produto
 fig_prod = px.bar(df_filtred, x='Date', y='Product line', color='City', title='Faturamento por tipo de produto', orientation='h')
-col2.plotly_chart(fig_prod)
+col2.plotly_chart(fig_prod, use_container_width=True)
 
 
+# Contruibuição por filial
 city_total = df_filtred.groupby('City')[['Total']].sum().reset_index()
+fig_city = px.bar(city_total, x='City', y='Total', title='Faturamento por filial')
+col3.plotly_chart(fig_city, use_container_width=True)
 
-fig_city = px.bar(df_filtred, x='City', y='Total', title='Faturamento por tipo de produto', orientation='h')
-col3.plotly_chart(fig_city)
+
+
+# Contruibuição por filial
+city_total = df_filtred.groupby('City')[['Total']].sum().reset_index()
+fig_king = px.pie(df_filtred, values='Total', names='Payment', title='Faturamento por tipo de pagamento')
+col4.plotly_chart(fig_king, use_container_width=True)
+
+
+# Contruibuição por filial
+city_total = df_filtred.groupby('City')[['Total']].sum().reset_index()
+fig_rating = px.bar(df_filtred, x='City', y='Rating', title='Avaliação')
+col5.plotly_chart(fig_rating, use_container_width=True)
